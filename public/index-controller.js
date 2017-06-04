d2c5c48803c833c21928fcb2d358fe1b21e67978
Victor Kazuyuki Shinya <contatovictorshinya@gmail.com>
2017-06-04 10:05:48 -0300

    Major changes

--- public/index-controller.js
+++ public/index-controller.js
@@ -10,6 +10,13 @@ app.controller('indexCtrl', function($scope) {
         "nome": " INSTITUI O PROGRAMA DE PARCELAMENTO INCENTIVADO DE 2017- PPI 2017"
     },
     {
+        "resultado": "Reprovado",
+        "presentes": "55",
+        "curtidas": 8,
+        "rejeicoes": 7,
+        "nome": "Emendas de 2 a 10, 12 e 13, de 15 a 20, 22 a 25, e de 27 a 36"
+    },
+    {
         "resultado": "Aprovado",
         "presentes": "51",
         "curtidas": 5,
@@ -27,13 +34,6 @@ app.controller('indexCtrl', function($scope) {
         "curtidas": 7,
         "rejeicoes": 6,
         "nome": " DISPÕE SOBRE A APLICAÇÃO DO ART. 1º DA LEI Nº 14.889, DE 20 DE JANEIRO DE 2009, E DÁ OUTRAS PROVIDÊNCIAS. (FICAM ATUALIZADOS MONETARIAMENTE EM 4,76% (QUATRO E SETENTA E SEIS CENTÉSIMOS PERCENTUAIS), A PARTIR DO DIA 1º DE MARÇO DE 2017 OS VENCIMENTOS, FUNÇÕES GRATIFICADAS, SALÁRIOS, SALÁRIO-FAMÍLIA E SALÁRIO-ESPOSA DOS SERVIDORES PÚBLICOS DA CÂMARA MUNICIPAL DE SÃO PAULO)."
-    },
-    {
-        "resultado": "Reprovado",
-        "presentes": "55",
-        "curtidas": 8,
-        "rejeicoes": 7,
-        "nome": "Emendas de 2 a 10, 12 e 13, de 15 a 20, 22 a 25, e de 27 a 36"
     },
     {
         "resultado": "Reprovado",

c4f5fa074e8f56efdc20a44f1e026083dd78eb3a
Victor Kazuyuki Shinya <contatovictorshinya@gmail.com>
2017-06-03 22:32:14 -0300

    Moved to public folder

---
+++ public/index-controller.js
@@ -0,0 +1,5 @@
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope) {
+    $scope.firstName = "Qualidade";
+    $scope.lastName = "Catossi";
+});
---
+++ public/vereador/index-controller.js
@@ -0,0 +1,4 @@
+var app = angular.module('myApp', []);
+app.controller('vereadorCtrl', function($scope) {
+    $scope.message = "Hello world";
+});
